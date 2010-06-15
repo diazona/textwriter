af8129bb8710d1fff1750da73cf5d05dda8376e8
David Zaslavsky <diazona@ellipsix.net>
2010-06-15 01:37:45 -0400

    Merge content of RenderResponse into RenderRequest

--- src/net/ellipsix/textwriter/RenderRequest.java
+++ src/net/ellipsix/textwriter/RenderRequest.java
@@ -27,7 +27,15 @@ package net.ellipsix.textwriter;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.RasterFormatException;
 import java.io.BufferedReader;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
 
 /**
  * A text rendering request as received from the Textwriter frontend.
@@ -35,28 +43,11 @@ import java.io.BufferedReader;
  * @author David Zaslavsky
  */
 public class RenderRequest {
-    /**
-     * Parses a color from a 8-character hex digit specification.
-     *
-     * This is almost the same thing that {@link Color#decode(String)} does,
-     * except that this method handles an alpha channel.
-     *
-     * @param color the string specification of the color, which must contain
-     *  exactly 8 hexadecimal digits
-     * @return the Color object corresponding to the input string
-     */
-    protected static Color parseColor(String color) {
-        int red = Integer.parseInt(color.substring(0, 2), 16);
-        int green = Integer.parseInt(color.substring(2, 4), 16);
-        int blue = Integer.parseInt(color.substring(4, 6), 16);
-        int alpha = Integer.parseInt(color.substring(6, 8), 16);
-        return new Color(red, green, blue, alpha);
-    }
-
-    String text;
-    Font font;
-    Color background;
-    Color foreground;
+    private String text;
+    private Font font;
+    private Color background;
+    private Color foreground;
+    private BufferedImage image;
 
     public static RenderRequest parse(BufferedReader r) {
         String fontName = r.readLine();
@@ -79,10 +70,79 @@ public class RenderRequest {
         return new RenderRequest(text, font, parseColor(background), parseColor(foreground));
     }
 
-    private RenderRequest(String text, Font font, Color background, Color foreground) {
+    public RenderRequest(String text, Font font, Color background, Color foreground) {
         this.text = text;
         this.font = font;
         this.background = background;
         this.foreground = foreground;
+        this.image = renderText(text, font, background, foreground);
+    }
+
+    public void write(OutputStream out) {
+        ImageIO.write(image, "PNG", out);
+    }
+
+    public void write(File file) {
+        ImageIO.write(image, "PNG", file);
+    }
+
+    /**
+     * Creates an image with the given text using the given rendering parameters.
+     *
+     * @param text the text to draw in the image
+     * @param font the font to use to draw the text
+     * @param bgColor the background color
+     * @param fgColor the foreground color
+     */
+    public static BufferedImage renderText(String text, Font font, Color bgColor, Color fgColor) {
+        // create the image
+        BufferedImage image = new BufferedImage(font.getSize() * text.length() + 2, font.getSize() + 2, BufferedImage.TYPE_4BYTE_ABGR);
+        
+        // get a graphics object
+        Graphics2D g = image.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        
+        // get the text boundary
+        TextLayout layout = new TextLayout(text, font, g.getFontRenderContext());
+        Rectangle2D bounds = layout.getBounds();
+        
+        // fill the background
+        g.setColor(bgColor);
+        g.fillRect(0, 0, (int)(bounds.getWidth() + 1), (int)(bounds.getHeight() + 1));
+        
+        // draw the text
+        g.setColor(fgColor);
+        layout.draw(g, -(float)bounds.getX(), -(float)bounds.getY());
+        
+        // and trim
+        try {
+            image = image.getSubimage(0, 0, (int)(bounds.getWidth() + 1), (int)(bounds.getHeight() + 1));
+        }
+        catch (RasterFormatException rfe) {
+//             log("Error in trimming image: " + rfe.getMessage());
+            // usually means that the text boundary was bigger than the canvas
+            // ignore for now
+        }
+
+//         log("created image with text '" + text + "'");
+        return image;
+    }
+
+    /**
+     * Parses a color from a 8-character hex digit specification.
+     *
+     * This is almost the same thing that {@link Color#decode(String)} does,
+     * except that this method handles an alpha channel.
+     *
+     * @param color the string specification of the color, which must contain
+     *  exactly 8 hexadecimal digits
+     * @return the Color object corresponding to the input string
+     */
+    protected static Color parseColor(String color) {
+        int red = Integer.parseInt(color.substring(0, 2), 16);
+        int green = Integer.parseInt(color.substring(2, 4), 16);
+        int blue = Integer.parseInt(color.substring(4, 6), 16);
+        int alpha = Integer.parseInt(color.substring(6, 8), 16);
+        return new Color(red, green, blue, alpha);
     }
 }
